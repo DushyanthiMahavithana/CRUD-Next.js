80b9726088a6abdc45a0082c3c8e3b3b61b1d0d4
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-10-31 16:56:17 +0530

    new changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar(){
     return(
         <nav className="flex justify-between items-cente bg-amber-200 px-10 py-4 my-8">
-            <Link className="text-orange-600 font-bold" href={"/"}>MY CRUD APP</Link>
-            <Link className=" bg-white p-3 text-orange-600 font-bold" href={"/addTask"}> ADD TASK</Link>
+            <Link className="text-orange-600 font-bold text-4xl" href={"/"}>MY CRUD APP</Link>
+            <Link className=" bg-white p-3 rounded-lg text-orange-600 font-bold" href={"/addTask"}> ADD TASK</Link>
         </nav>
 
     );
