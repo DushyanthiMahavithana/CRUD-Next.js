18edacecb089e1616ed3d837f446ae831022c04f
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-02 11:16:32 +0530

    created delete method

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -16,3 +16,16 @@ export async function GET (){
     const topic = await Topic.find();
     return NextResponse.json({topic});
 }
+
+
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongodb();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({message:"Deleted.."}, {status: 200});
+
+}
+
+
+
+
