7f13b663b6c1e68c8e8ba43fc0d710f813063a1f
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-05 19:21:19 +0530

    Refresh page automatically  after deleting

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
 
 export default function RemoveBtn ({id}){
+    const router = useRouter();
     const removeTopic = async () => {
         const confirmed = confirm("Are You Sure?");
 
         if(confirmed){
-            await fetch(`http://localhost:3000/api/topics?id=${id}`,{
+            const res = await fetch(`http://localhost:3000/api/topics?id=${id}`,{
                 method: "DELETE",
             });
+            
+            if(res.ok){
+            router.refresh();
+            }
         }
     };
 
