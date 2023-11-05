2add7a037033e31ebd58004d05e05cb5c1bb05a2
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-05 18:16:00 +0530

    Add task part use client

--- app/addTask/page.jsx
+++ app/addTask/page.jsx
@@ -1,11 +1,51 @@
+"use client";
+import {useState} from "react";
+
+
 export default function AddTask (){
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if(!title || !description){
+            alert("Title and description are required")
+            return;
+        }
+
+        try{
+            await fetch("http://localhost:3000/api/topics",{
+                method:"POST",
+                headers:{
+                    "content-type": "application/json"
+                },
+            });
+
+        }catch(error){
+
+        }
+
+    };
+
+
     return(
-        <form className="flex flex-col gap-3">
-            <input className="border-8 mx-20 px-8 py-3" type="text" placeholder="Task Name">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <input
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title}
+            className="border-8 mx-20 px-8 py-3" type="text" placeholder="Task Name">
             </input>
-            <input className="border-8 mx-20 px-8 py-3" type="text" placeholder="Description">
+
+            <input 
+             onChange={(e) => setDescription(e.target.value)} 
+             value={description}
+            className="border-8 mx-20 px-8 py-3" type="text" placeholder="Description">
             </input>
-            <button className="bg-orange-600 text-white font-bold py-3 px-10 w-fit mx-20">
+
+            <button 
+            type = "submit"
+            className="bg-orange-600 text-white font-bold py-3 px-10 w-fit mx-20">
                 ADD
             </button>
         </form>
