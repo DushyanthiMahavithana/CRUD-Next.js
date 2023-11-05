64055a18fbae0378b643c85a4b425d27e70d9cd6
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-05 10:18:49 +0530

    Connecting to the fronend

--- components/Topics.jsx
+++ components/Topics.jsx
@@ -2,21 +2,43 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function Topics(){
+
+const getTitle = async() =>{
+    try{
+        const res = await fetch ("http://localhost:3000/api/topics", {
+            cache: "no-store",
+        });
+        if(!res.ok){
+            throw new Error("Failed to Fetch Topics...");
+        }
+        return res.json();
+
+    }catch (error){
+        console.log("Error Loading Topics...",error);   
+
+    }
+}
+
+export default async function Topics(){
+
+    const {topics} = await getTitle();
+
     return (
         <>
+        {topics && topics.map ((t) => (
         <div className="p-4 border border-gray-500 my-3 flex justify-between gap-5">
             <div>
-                <h2 className="text-3xl font-bold">Task Title</h2>
-                <div>Task Description</div>
+                <h2 className="text-3xl font-bold">{t.title}</h2>
+                <div>{t.description}</div>
             </div>
             <div>
                 <RemoveBtn />
-                <Link href={'/editTopic/012'}>
+                <Link href={`/editTopic/${t._id}`}>
                     <HiPencilAlt size={24}/>
                 </Link>
             </div>
         </div>
+        ))}
         </>
     )
 }   
