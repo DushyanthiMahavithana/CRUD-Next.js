581ac749747474bf6f0377d2107bbb33fd7c55b3
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-05 20:08:38 +0530

    update task

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,5 +1,24 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function editTopic() {
-    return  <EditTopicForm />;
+const getTopicById = async(id) =>{
+    try{
+        const res = await fetch (`http://localhost:3000/api/topics/${id}`, {cache: "no-store"});
+
+        if (!res.ok){
+           throw new Error("Failed to Fetch")
+        }
+        return res.json();
+
+
+    }catch(error){
+        console.log(error);
+
+    }
+}
+
+export default async function editTopic({params}) {
+    const {id} = params;
+    const {topic} = await getTopicById(id);
+    const {title, description} = topic;
+    return  <EditTopicForm  id={id} title={title} description={description}/>;
 }
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,10 +1,54 @@
-export default function EditTopicForm (){
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function EditTopicForm ({id, title, description}){
+    const [newTitle, setNewTitle]=useState(title);
+    const [newDescription, setNewDescription]=useState(description);
+
+    const router = useRouter();
+
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        try{
+            const res = await fetch (`http://localhost:3000/api/topics/${id}`,{
+                method: "PUT",
+                headers:{
+                    "Content-type": "application/json"
+                },
+                body:JSON.stringify({newTitle,newDescription}),  
+            });
+            if (!res.ok){
+                throw new Error("Failed to Update")
+            }
+
+            router.refresh();
+            router.push("/"); 
+            
+        }catch(error){
+            console.log(error);
+
+        }
+    };
+
     return(
-        <form className="flex flex-col gap-3">
-            <input className="border-8 mx-20 px-8 py-3" type="text" placeholder="Task Name">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <input 
+            onChange={(e)=>setNewTitle(e.target.value)}
+            value={newTitle}
+            className="border-8 mx-20 px-8 py-3" type="text" placeholder="Task Name">
             </input>
-            <input className="border-8 mx-20 px-8 py-3" type="text" placeholder="Description">
+
+            <input 
+            onChange={(e)=>setNewDescription(e.target.value)}
+            value={newDescription}
+            className="border-8 mx-20 px-8 py-3" type="text" placeholder="Description">
             </input>
+
             <button className="bg-orange-600 text-white font-bold py-3 px-10 w-fit mx-20">
                 UPDATE
             </button>
@@ -12,4 +56,5 @@ export default function EditTopicForm (){
 
     );
     
-}
+    
+};
